b6fc433b314fe2c3ad8a5f721a6d2b95d8293c38
Wxxlfe <142287140+Wxxlfe@users.noreply.github.com>
2023-12-04 19:45:36 -0700

    added/deleted files in the controller folder and linked routes to models

---
+++ controllers/api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const carRoutes = require('./vehicleRoutes');
+const partsRoutes = require('./partsRoutes');
+const messageRoutes = require('./MessageRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/car', carRoutes);
+router.use('/parts', partsRoutes);
+router.use('/message', messageRoutes);
+router.use('/user', userRoutes);
+
+module.exports = router;
+
--- controllers/api/vehicleRoutes.js
+++ controllers/api/vehicleRoutes.js
@@ -1,3 +1,30 @@
 // This is a file that is meant to change as more of the client side of the web application is shown through the MVC/ static page
 // My thoughts on what interactive routes would be needed for our webapplication based of our goal
 // This would handle posting a new car onto the data base for the web application 
+const router = require('express').Router();
+const { Vehicles } = require('../../models');
+
+
+
+router.post('/', async (req, res) => {
+    try {
+      const vehiclesData = await Vehicles.create(req.body);
+  
+      req.session.save(() => {
+        req.session.user_id = vehiclesData.userid;
+        req.session.logged_in = true;
+  
+        res.status(200).json(vehiclesData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+
+  
+  
+  
+  
+  module.exports = router;
+  
