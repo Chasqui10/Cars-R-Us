f926730e24aa586d958329b71cf48eb8dbdbf129
Chasqui10 <chasqui.rmf@gmail.com>
2023-12-06 12:38:29 -0700

    Login is now linking to profile view (handlebars)

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -11,7 +11,7 @@ router.post('/', async (req, res) => {
     const userData = await User.create(req.body);
 
     req.session.save(() => {
-      req.session.user_id = userData.userid;
+      req.session.userid = userData.userid;
       req.session.logged_in = true;
 
       res.status(200).json(userData);
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
     }
 
     req.session.save(() => {
-      req.session.user_id = userData.userid;
+      req.session.userid = userData.userid;
       req.session.logged_in = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
