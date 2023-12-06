8ee7f31eae1206d03dac5e9a2e646a8b45ad5011
= <the.digital.avatar@gmail.com>
2023-12-05 23:02:28 -0700

    Added static route and commented out the bad one

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,30 +2,34 @@ const router = require('express').Router();
 const { User, Inventory } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    const inventoryData = await Inventory.findall({
-      include: [
-        {
-          model: User,
-          attributes: ['name']
-        },
-      ],
-    });
+// router.get('/', async (req, res) => {
+//   try {
+//     const inventoryData = await Inventory.findall({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['id']
+//         },
+//       ],
+//     });
 
-    const inventory = inventoryData.map((invent) => invent.get({ plain: true })); 
+//     const inventory = inventoryData.map((invent) => invent.get({ plain: true })); 
     
-    // Send the rendered Handlebars.js template back as the response
-    res.render('index', {
-      inventory,
-      logged_in: req.session.logged_in
-    });
+//     // Send the rendered Handlebars.js template back as the response
+//     res.render('index', {
+//       inventory,
+//       logged_in: req.session.logged_in
+//     });
 
-  } catch (err) {
-    res.status(500).json(err);
-  }
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
 
-});
+// });
+
+router.get('/', async (req, res) => {
+ res.render('landing');
+});  
 
 router.get('/profile', withAuth, async (req, res) => {
     try {
